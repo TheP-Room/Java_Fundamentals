2473896ee806569d4f7bcb0d3f366189edd7f475
TheP <dev.ballam@gmail.com>
2025-04-08 23:48:19 +0530

    MortrageCalculatorTypes.java

--- MortrageCalculatorTypes.java
+++ MortrageCalculatorTypes.java
@@ -19,3 +19,27 @@ public class MortrageCalculatorTypes {
         //472.81 with p = 100000 , i = 3.92 , y = 30
     }
 }
+
+// Alternate Method
+/*
+import java.text.NumberFormat;
+import java.util.Scanner;
+import java.lang.Math;
+public class Revision {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Principal: ");
+        double principal = sc.nextDouble();
+        System.out.print("Annual Intrest Rate: ");
+        float intrest = sc.nextFloat();
+        intrest /= 1200;
+        System.out.print("Period (Years): ");
+        byte years = sc.nextByte();
+        double months = years*12;
+        double num = principal*intrest*Math.pow((1+intrest),months);
+        double mortgage = num/(Math.pow((1+intrest),months)-1);
+        System.out.print("Mortgage: "+NumberFormat.getCurrencyInstance().format(mortgage));
+    }
+}
+
+*/
